473d365e17077a1d79156da8d429b123f3bbe1b3
katie-zhong <tokatiezhong@gmail.com>
2022-06-23 20:51:37 -0400

    Added new analysis

--- testScript.R
+++ testScript.R
@@ -1,7 +1,19 @@
-library(police)
+# library(police)
+# 
+# police |>
+#   select(summary, po_race, district_occurrence, investigative_findings) |>
+#   
+#   mutate(race = str_detect(po_race, pattern = "rac")) |>
+#   filter(police, race == TRUE)
 
-police |>
-  select(summary, po_race, district_occurrence, investigative_findings) |>
+nhanes |>
+  mutate(weight = weight*2.2) |>
+  mutate(height = height/30.48) |>
   
-  mutate(race = str_detect(po_race, pattern = "rac")) |>
-  filter(police, race == TRUE)
+  select(gender, weight, height, bmi) |>
+  drop_na(weight, height) |>
+  
+  gg_plot(aes(x = weight,
+              y = height,
+              color = bmi)) +
+  geom_jitter()
